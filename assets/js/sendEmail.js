9adad23ef05f9f365355f929803084bb7c7e5c4c
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-06-26 22:14:30 +0000

    Add javascript code for form post method and fix bugs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,13 +1,15 @@
 var templateParams = {
-    from_name: 'name',
-    from_email: 'email',
-    notes: 'The question I want to ask is...'
-};
- 
-emailjs.send('service_oxs23un', 'template_yu7s4up', templateParams)
+    'from_name': contactForm.name.value,
+    'from_email': contactForm.emailaddress.value,
+    'farmhouse_booking': contactForm.booking-info.value
+   };
+function sendMail(contactForm){
+   emailjs.send('service_oxs23un', 'template_yu7s4up', templateParams)
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
     }, function(error) {
        console.log('FAILED...', error);
-    });
-
+      }
+   );return false; 
+   
+}
